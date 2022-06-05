f411c370f56ba3741ec6c8bb84a534b21d3139a1
Catherine Kim <80084168+catherinek07@users.noreply.github.com>
2022-06-05 16:51:58 -0600

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,10 +1,12 @@
-	let status = 'draw'; 
+	let status = 'draw'; // holds current state in relation to draw, erase, fill tools.
 	let col;
 	let row;
 	let isMouseDown = false;
 	let color;
 	const MAX_ALLOWED_HEIGHT = 30;
 	const MAX_ALLOWED_WIDTH = 30;
+
+	// makeGrid function to create pixel art grid
 	function makeGrid() {
 		row = $('#input_height').val();
 		col = $('#input_width').val();
@@ -24,15 +26,24 @@
 		});
 	};
 
+	// button event to create new grid
 	$('#size_picker').on('submit', function(e) {
 		e.preventDefault();
 		$('#pixel_canvas').empty();
 		makeGrid();
 	});
 
+	// click event to add / erase / get colour to/from grid elements,
+	// and also allowing to drag across multiple cells
 	$('#pixel_canvas').on('mousedown', 'td', function(e) {
     e.preventDefault();
 		isMouseDown = true;
+
+ 		// *** this is starter code that can be uncommented and used when dropper 'getColour' function is added
+		// if(status === 'getColour'){
+		// 	let tempColour = $(this).css('background-color');
+		// 	$('input#color_picker').val(rgbToHex(tempColour));	
+		// }
     
 		color = $('input#color_picker').val(); // ***May need to change this when colour palettes added
 
@@ -54,25 +65,31 @@
           isMouseDown = false;
     });
 
+	// shortcut right-click event to remove colour from grid element
 	$('#pixel_canvas').on('contextmenu', 'td', function(e) {
 		e.preventDefault(); // stops context menu popping up on right-click
 		$(this).removeAttr('style');
 	});
 
+	// click border toggle button to toggle borders on the grid
 	$('.borderToggleBtn').on('click', function(){
 		$('tr, td').toggleClass('transparentBorder');
 	});
 
+	// function to clear the content of the canvas grid
 	$('.clearGrid').on('click', function(){
 		$('td').removeAttr('style');
 	});
 
+	// add Column to grid
 	$('.addCol').on('click', function(){
 		if(col < MAX_ALLOWED_WIDTH){
 			$('tr').append('<td></td>');
 			col++;
 		}
 	});
+
+	// remove Column from grid
 	$('.removeCol').on('click', function(){
 		if(col > 0){
 			$('tr td:last-child').remove();
@@ -80,6 +97,7 @@
 		}
 	});
 
+	// add Row to grid
 	$('.addRow').on('click', function(){
 		if(row < MAX_ALLOWED_HEIGHT){
 			row++;
@@ -99,6 +117,7 @@
 		}
 	});
 
+	// Status button functions
 	$('.draw').click(function(){
 		status = 'draw';
 	});
@@ -111,6 +130,7 @@
 		status = 'erase';
 	});
 
+	// Function to handle Active tool styling
 	$('.tools').click(function(){
 		$('.tools').removeClass('active');
 		$(this).addClass('active');
@@ -127,4 +147,17 @@
 			}
 		});
 	});
-    // Future: NFTs?
+
+	// TODO - add dropper function to get colour from a cell
+	// TODO - add colour palettes in right-hand column
+	// TODO - feature to share image on social media ???
+	
+	// 'getColour' function
+	// $('.getColour').click(function(){
+	// 	status = 'getColour';
+	//	...
+	// });
+
+	// function to convert RGB colour value to Hex so it can be inserted
+	// as a value to the colour input
+	//function rgbToHex(){};
